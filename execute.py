bd128d6617567ae484b24b5717b0997e30418a3a
samantha <1342061060@qq.com>
2019-08-23 10:50:56 +0800

    START (8.23)

--- execute.py
+++ execute.py
@@ -1,9 +1,10 @@
 # coding=utf-8
 
+# import numpy as np
+# import controls
 import argparse
 import time
 import logging
-from sympy import *
 
 logging.basicConfig(filename='/home/samantha/_train_/logs/execute.log', level=logging.INFO)
 logging.info('\n')
@@ -17,121 +18,24 @@ def parse_args():
     return parser.parse_args()
 
 
-def make_env(scenario_name, arglist):
-    from multiagent.environment import MultiAgentEnv
-    import multiagent.scenarios as scenarios
+def make_env(arglist):
+    from MAEnv.environment import MultiAgentEnv
+    import MAEnv.scenarios as scenarios
 
     # load scenario from script
-    scenario = scenarios.load(scenario_name + ".py").Scenario()
+    scenario = scenarios.load(arglist.scenario + ".py").Scenario()  # 建立一个类（？）
     # create world
     world = scenario.make_world()
     # create multiagent environment
     env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.observation)
-    return env
+    return env, world
 
 
-def rule1(obs_n, upl0, downl0, bar, march, num):
-    action_n = []
-    k = 0
-    for obs in obs_n:
-        border = get_border(upl0, downl0, bar, march, num, k)
-        action = get_action(obs, border, k)
-        action_n.append(action)
-        k += 1
-    print('obs_n', obs_n)
-    print('tt', tt)
-    print('ss', ss)
-    print(action_n)
-    return action_n
-
-
-def rule2(obs_n, upl0, downl0, bar, march, num, L1):
-    action_n = []
-    global pointA, pointB
-    k = 0
-    for obs in obs_n:
-        border = get_border(upl0, downl0, bar, march, num, k)
-        [pointA[k], pointB[k]] = funWP(obs, border, k, L1)
-        action = funL1(obs, pointA[k], pointB[k], L1)
-        action_n.append(action)
-        k += 1
-    return action_n
-
-
-def funWP(obs, border, k, L1):
-    pointAk = (-0.9, 0.9)
-    pointBk = (0.9, 0.9)
-    return [pointAk, pointBk]
-
-
-def funL1(obs, pointAk, pointBk, L1):
-    x, y = symbols('x y')
-    [xk, yk] = obs[2:4]
-    (xA, yA) = pointAk
-    (xB, yB) = pointBk
-    out = solve([Eq((xk-x)*(xk-x) + (yk-y)*(yk-y), L1*L1),
-                Eq((xA-x)*(yA-yB), (yA-y)*(xA-xB))],
-                [x, y])
-    action = [0, 0, 0, 0, 0]
-    return action
-
-
-def get_border(upl0, downl0, bar, march, num, k):
-    upl = upl0 - bar * k
-    downl = downl0 + bar * (num - 1 - k)
-    rightlu = (2 * num - 2 * k - 1) * march
-    rightld = (2 * k + 1) * march
-    return upl, downl, rightlu, rightld
-
-    # action = [0, 0, 0, 1, 0]  # up
-    # action = [0, 1, 0, 0, 0]  # right
-    # action = [0, 0, 0, 0, 1]  # down
-
-
-def get_action(obs, border, k):
-    upl = border[0]
-    downl = border[1]
-    rightlu = border[2]
-    rightld = border[3]
-    action = [0, 1, 0, 0, 0]
-    global tt, ss
-
-    if tt[k] == 1:
-        ss[k] += 1
-        if obs[3] > 0 and ss[k] > rightlu:
-            action = [0, 0, 0, 0, 1]
-            tt[k] = 0
-            print('ops')
-        if obs[3] <= 0 and ss[k] > rightld:
-            action = [0, 0, 0, 1, 0]
-            tt[k] = 0
-            print('ops')
-
-    else:
-        if obs[1] > 0:
-            if obs[3] < upl:
-                action = [0, 0, 0, 1, 0]
-            else:
-                action = [0, 1, 0, 0, 0]
-                tt[k] = 1
-                ss[k] = 0
-                print('come')
-        if obs[1] <= 0:
-            if obs[3] > downl:
-                action = [0, 0, 0, 0, 1]
-            else:
-                action = [0, 1, 0, 0, 0]
-                tt[k] = 1
-                ss[k] = 0
-                print('come')
-
-    return action
-
-
-def execute(arglist):
+if __name__ == '__main__':
+    arglist = parse_args()
 
     # Create environment
-    env = make_env(arglist.scenario, arglist)
+    env, world = make_env(arglist)
     obs_n = env.reset()
     episode_step = 0
 
@@ -164,8 +68,3 @@ def execute(arglist):
         # for displaying
         time.sleep(0.05)
         env.render()
-
-
-if __name__ == '__main__':
-    arglist = parse_args()
-    execute(arglist)
