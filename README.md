598b045aad1438423affc28a293eaf740ef3d4df
samantha <1342061060@qq.com>
2019-08-26 15:50:11 +0800

    cleanup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Groupy
+# MAControl
+
 ：）
--- execute.py
+++ execute.py
@@ -1,20 +1,19 @@
 # coding=utf-8
-# This is the main file
+
 import argparse
 import time
-# import controller
 import MAControl.TESTControl as TESTC
 
 
 def parse_args():
+
     parser = argparse.ArgumentParser("Control Experiments for Multi-Agent Environments")
     parser.add_argument("--scenario", type=str, default="scenario_DIY", help="name of the scenario script")
-    parser.add_argument("--step-max", type=int, default=4000, help="maximum steps")
+
     return parser.parse_args()
 
 
 def make_env(arglist):
-    print('make_env')
     from MAEnv.environment import MultiAgentEnv
     import MAEnv.scenarios as scenarios
 
@@ -24,13 +23,15 @@ def make_env(arglist):
     # create world and env
     world = scenario.make_world()
     env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.observation)
+
     return env, world
 
 
 if __name__ == '__main__':
+
     arglist = parse_args()
 
-# xin
+    # init waypoint
     init_waypoint = [[-0.9, -0.9, 1],
                      [-0.9, 0.9, 1],
                      [0.9, 0.9, 1],
@@ -47,7 +48,6 @@ if __name__ == '__main__':
 
     obs_n = env.reset()
     step = 0
-    start = time.time()
 
     while True:
 
@@ -55,8 +55,8 @@ if __name__ == '__main__':
         action_n = []
         for i in range(env.n):
             (pointAi, pointBi) = Control[i].PathPlanner(obs_n[i])
-            action = Control[i].MotionController(obs_n[i], pointAi, pointBi)
-            action_n.append(action)
+            actioni = Control[i].MotionController(obs_n[i], pointAi, pointBi)
+            action_n.append(actioni)
 
         # environment step
         new_obs_n, rew_n, done_n, info_n = env.step(action_n)
