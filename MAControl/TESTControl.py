8fa15b1368a4eb919b90abdb4b4b48c49783e7a6
samantha <1342061060@qq.com>
2019-08-28 15:32:23 +0800

    samantha:L1

--- MAControl/TESTControl.py
+++ MAControl/TESTControl.py
@@ -20,7 +20,7 @@ class TESTControl():
         self.L1_distance = 0
         self.nav_bearing = 0
         self.eta = 0
-        self.lateral_accel = 0
+        self.lateral_acc = 0
 
         self.action = [0, 0, 0, 0, 0]
 
@@ -74,36 +74,29 @@ class TESTControl():
         if dist_AP > L1_distance and alongTrackDist/max(dist_AP, 0.001) < -0.707:
             # calculate eta to fly to waypoint A
             eta = math.acos(np.dot(-1 * vector_AP_unit, self.vel/speed))
-            nav_bearing = math.atan2(vector_AP[1], vector_AP[0])
 
         elif abs(AB_to_BP_bearing) < math.radians(100):
             # calculate eta to fly to waypoint B
             eta = math.acos(np.dot(-1 * vector_BP_unit, self.vel/speed))
-            nav_bearing = math.atan2(vector_BP[1], vector_BP[0])
 
         else:
             # calculate eta to fly along the line between A and B
             eta2 = math.acos(np.dot(vector_AB, self.vel/speed))
             beta = math.acos(np.dot(vector_AP_unit, vector_AB_unit))
             xtrackErr = dist_AP * math.sin(beta)
-            eta1 =
-            vector_AP =
-            sin_eta1 = xtrackErr / max(L1_distance, 0.001)
-            #  TODO: (c++): sin_eta1 = math::constrain(sin_eta1,-0.7071f,0.7071f)
-            eta1 = math.asin(sin_eta1)
+            sine_eta1 = xtrackErr / max(L1_distance, 0.001)
+            #  TODO: (c++): sine_eta1 = math::constrain(sine_eta1,-0.7071,0.7071)
+            eta1 = math.asin(sine_eta1)
             eta = eta1 + eta2
-            nav_bearing = math.atan2(vector_AB(1), vector_AB(0)) + eta1
 
         #  TODO: (c++): eta = math::constrain(eta,-pi/2,pi/2)
-        KL1 = 0.02
-        self.lateral_accel = KL1 * groundspeed * groundspeed / L1_distance * math.sin(eta)
-        circle_mode = false
-        bearing_error = eta
+        self.lateral_acc = speed * speed / L1_distance * math.sin(eta) * 0.02  # K_L1
 
-        accel_unit = np.array([self.vel[1], -1*self.vel[0]])
-        accel_command = accel_unit * self.lateral_accel
-        self.action[2] = accel_command[0]
-        self.action[4] = accel_command[1]
+        lateral = np.array([self.vel[1], -1*self.vel[0]])
+        acc = lateral * self.lateral_acc
+        self.action = [0, 0, 0, 0, 0]
+        self.action[2] = acc[0]
+        self.action[4] = acc[1]
         return self.action
 
 
