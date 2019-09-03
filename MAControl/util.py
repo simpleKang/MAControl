44882e24d7d88231352286f6eda4c0641a6857f9
wzq <you@you>
2019-09-03 09:12:46 +0800

    蛇形航点改进

--- MAControl/util.py
+++ MAControl/util.py
@@ -11,26 +11,24 @@ def snake_single(Up, Down, Wide1, Wide2, Edge, W, D, i):
     wp_list = []
     point = [-W+i*D, -W, 1]
     wp_list.append(point)
-    i = 1
     while True:
-        Re = i % 4
-        if Re==1:
-            point = [point[0], Up, 1]
-            wp_list.append(point)
-        elif Re==2:
-            point = [point[0]+Wide1, Up, 1]
-            if point[0] > Edge:
-                break
-            wp_list.append(point)
-        elif Re==3:
-            point = [point[0], Down, 1]
-            wp_list.append(point)
-        else:
-            point = [point[0]+Wide2, Down, 1]
-            if point[0] > Edge:
-                break
-            wp_list.append(point)
-        i += 1
+
+        point = [point[0], Up, 1]
+        wp_list.append(point)
+
+        point = [point[0]+Wide1, Up, 1]
+        if point[0] > Edge:
+            break
+        wp_list.append(point)
+
+        point = [point[0], Down, 1]
+        wp_list.append(point)
+
+        point = [point[0]+Wide2, Down, 1]
+        if point[0] > Edge:
+            break
+        wp_list.append(point)
+
     return wp_list
 
 
@@ -66,6 +64,7 @@ for i in range(N):
     Wide1.append(D*(2*(N-i)-1))
     Wide2.append(D*(2*i+1))
     init_waypoint.append(snake_single(Up[i], Down[i], Wide1[i], Wide2[i], Edge, W, D, i))
+
 
 
 
