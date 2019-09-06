efd6a464aaa0a52eb3f1ca057326405629a04549
wzq <you@you>
2019-09-06 11:41:12 +0800

    吃饭前更新

--- MAControl/TESTControl.py
+++ MAControl/TESTControl.py
@@ -158,11 +158,12 @@ class TESTControl(object):
 
                 # 目标状态为0时进行拍卖者的选择，所有人都会进来
                 if TESTControl.target_relist[TESTControl.Target_index][2] == 0:
-                    # TODO 添加判断条件，正在执行的小飞机不能被选择
-                    if k in TESTControl.Found_Target_Info[TESTControl.Target_index]:
+                    # 正在执行的小飞机不能被选择
+                    if k in TESTControl.Found_Target_Info[TESTControl.Target_index] and TESTControl.Shared_UAV_state[k] != 3:
                         TESTControl.Select_list.append([k,
-                            math.sqrt((obs_n[k][2]-TESTControl.Found_Target_Set[TESTControl.Target_index][0])**2+
+                            math.sqrt((obs_n[k][2]-TESTControl.Found_Target_Set[TESTControl.Target_index][0])**2 +
                                       (obs_n[k][3]-TESTControl.Found_Target_Set[TESTControl.Target_index][1])**2)])
+                    # TODO ！！！由谁进行拍卖者选择列表的排序及选拍卖者的操作！！！
                     if k == (len(obs_n)-1):
                         TESTControl.Select_list = sorted(TESTControl.Select_list, key=lambda x: x[1])
                         TESTControl.Auctioneer = TESTControl.Select_list[0][0]
@@ -171,10 +172,10 @@ class TESTControl(object):
                 # 目标状态为1时进行拍卖者生成要发送的step延时,所有人都会进来，看看自己是不是拍卖者，是的话进行操作，确认竞拍者
                 elif TESTControl.target_relist[TESTControl.Target_index][2] == 1:
                     if k == TESTControl.Auctioneer:
-                        # TODO 添加判断条件，邻域内正在执行的小飞机不能成为竞拍者
                         for i in self.close_area:
                             # TODO 传输延时step个数的计算优化
-                            TESTControl.Trans_step.append([i, round(math.sqrt((obs_n[k][2]-obs_n[i][2])**2+(obs_n[k][3]-obs_n[i][3])**2)/0.05)])
+                            if TESTControl.Shared_UAV_state[i] != 3:
+                                TESTControl.Trans_step.append([i, round(math.sqrt((obs_n[k][2]-obs_n[i][2])**2+(obs_n[k][3]-obs_n[i][3])**2)/0.05)])
                         TESTControl.last_step = step
                         TESTControl.target_relist[TESTControl.Target_index][2] = 2
 
