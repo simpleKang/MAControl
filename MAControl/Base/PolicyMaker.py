6ed8b286afdef22add290bf97de090c88e6e914b
wzq <you@you>
2019-09-09 17:06:25 +0800

    初始化过程研究

--- MAControl/Base/PolicyMaker.py
+++ MAControl/Base/PolicyMaker.py
@@ -4,7 +4,11 @@ from abc import ABC, abstractmethod
 class PolicyMaker(ABC):
 
     def __init__(self, name, env, world, agent_index, arglist):
-        pass
+        self.name = name
+        self.env = env
+        self.world = world
+        self.index = agent_index
+        self.arglist = arglist
 
     @abstractmethod
     def makepolicy(self, WorldTarget, obs_n, step):
--- MAControl/Test_Auction/PolicyMaker_Auction.py
+++ MAControl/Test_Auction/PolicyMaker_Auction.py
@@ -5,14 +5,12 @@ class PolicyMaker_Auciton(PolicyMaker.PolicyMaker):
 
     def __init__(self, name, env, world, agent_index, arglist):
         super(PolicyMaker_Auciton, self).__init__()
-        self.name = name
-        self.env = env
-        self.world = world
-        self.index = agent_index
-        self.arglist = arglist
+        self.waypoint_list = []
+        self.cycle_index = 100
         pass
 
     def makepolicy(self, WorldTarget, obs_n, step):
-        print(self.index, ' This is a policymaker.')
+        print(self.index, self.cycle_index, ' This is a policymaker.')
+
         pass
 
