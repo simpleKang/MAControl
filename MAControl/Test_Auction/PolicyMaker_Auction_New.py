d007e660434cb7780b684d6ce4a67eb7b144b2af
samantha <1342061060@qq.com>
2019-09-11 05:53:50 +0800

    pretty

--- MAControl/Test_Auction/PolicyMaker_Auction_New.py
+++ MAControl/Test_Auction/PolicyMaker_Auction_New.py
@@ -157,6 +157,7 @@ class PolicyMaker_Auciton(PolicyMaker):
                             PolicyMaker_Auciton.Remain_Target_Set.append(PolicyMaker_Auciton.Found_Target_Set[i]+[i])
 
                     PolicyMaker_Auciton.Remain_Target_Set = sorted(PolicyMaker_Auciton.Remain_Target_Set, key=lambda x: x[4], reverse=True)
+                    print('Found_Target_Set: ', PolicyMaker_Auciton.Found_Target_Set)
                     print('Remain_Target_Set: ', PolicyMaker_Auciton.Remain_Target_Set)
 
             elif step == self.Step1:
