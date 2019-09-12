85f5f87ade3eb8d365fde4f741d948d849c39beb
samantha <1342061060@qq.com>
2019-09-12 10:37:50 +0800

    pretty

--- MAEnv/scenarios/TargetProfile.py
+++ MAEnv/scenarios/TargetProfile.py
@@ -5,9 +5,9 @@ num_targets = 3
 target_value = [7, 5, 2]
 target_size = [3, 3, 2]
 target_defence = [3, 3, 2]
-
 target_pos = [[-0.5, 0.9], [0, 0.9], [0.5, -0.5]]
+
 grid_pos = [[0, 0], [-1, 1], [-1, -1], [1, 1], [1, -1]]
+grid_color = np.array([0.25, 0.25, 0.25])
 
 agent_color = np.array([0.47, 0.79, 0.79])
-grid_color = np.array([0.25, 0.25, 0.25])
