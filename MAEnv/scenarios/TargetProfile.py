097ed4280af4285f088a74528a243a7468725c15
samantha <1342061060@qq.com>
2019-09-16 22:41:45 +0800

    view

--- MAEnv/scenarios/TargetProfile.py
+++ MAEnv/scenarios/TargetProfile.py
@@ -2,10 +2,10 @@ import numpy as np
 
 num_targets = 5
 
-target_value = [7, 5, 2, 3, 5]
-target_size = [2, 2, 2, 2, 2]
-target_defence = [1, 1, 1, 1, 1]
-target_pos = [[-0.5, 0.9], [0, 0.9], [0.5, -0.5],[0.0,0.0],[-0.9,-0.9]]
+target_value = [4, 2, 3, 1, 5]
+target_size = [4, 2, 3, 1, 5]  # 大小与价值相匹配
+target_defence = [4, 2, 3, 1, 5]  # 防御能力与价值相匹配
+target_pos = [[-0.5, 0.9], [0, 0.9], [0.5, -0.5], [0.0, 0.0], [-0.9, -0.9]]
 
 grid_pos = [[0, 0], [-1, 1], [-1, -1], [1, 1], [1, -1]]
 grid_color = np.array([0.25, 0.25, 0.25])
