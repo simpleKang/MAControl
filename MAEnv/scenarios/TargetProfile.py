feaad1f8c85146bb2035d300ebd5efbdf0ff146e
samantha <1342061060@qq.com>
2019-09-17 09:43:11 +0800

    targets

--- MAEnv/scenarios/TargetProfile.py
+++ MAEnv/scenarios/TargetProfile.py
@@ -1,11 +1,12 @@
 import numpy as np
 
-num_targets = 5
+num_targets = 6
 
-target_value = [4, 2, 3, 1, 5]
-target_size = [4, 2, 3, 1, 5]  # 大小与价值相匹配
-target_defence = [4, 2, 3, 1, 5]  # 防御能力与价值相匹配
-target_pos = [[-0.5, 0.9], [0, 0.9], [0.5, -0.5], [0.0, 0.0], [-0.9, -0.9]]
+target_type = [1, 1, 1, 1, 2, 3]  # 1=发射车 2=雷达 3=补给车
+target_value = [2, 2, 2, 2, 10, 5]
+target_size = [2, 2, 2, 2, 10, 5]  # 大小与价值相匹配
+target_defence = [5, 5, 5, 5, 1, 2]
+target_pos = [[-0.5, 0.9], [-0.5, 0.5], [0.0, 0.8], [0.0, 0.4], [-0.9, -0.9], [0.5, -0.5]]
 
 grid_pos = [[0, 0], [-1, 1], [-1, -1], [1, 1], [1, -1]]
 grid_color = np.array([0.25, 0.25, 0.25])
