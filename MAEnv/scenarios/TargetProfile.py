0b5800b7e0502be0e97c1622ae8f780a872a8619
samantha <1342061060@qq.com>
2019-10-16 10:16:39 +0800

    target pos temp

--- MAEnv/scenarios/TargetProfile.py
+++ MAEnv/scenarios/TargetProfile.py
@@ -4,10 +4,9 @@ num_targets = 6
 
 target_type = [1, 1, 1, 1, 2, 3]  # 1=发射车 2=雷达 3=补给车
 target_value = [2, 2, 2, 2, 10, 5]
-target_size = [2, 2, 2, 2, 10, 5]  # 大小与价值相匹配
+target_size = [2, 2, 2, 2, 2, 2]  # 大小相同
 target_defence = [5, 5, 5, 5, 1, 2]
-target_pos = [[0.68, 0.15], [0.36, 0.84], [0.85, -0.25], [0.49, -0.62], [0.54, -0.17], [0.32, -0.09]]
-# 生成方式 # a = np.random.choice(200, 12) # b = (a-100)/100
+target_pos = [list((np.random.choice(200, 2)-100)/100) for i in range(6)]
 
 grid_pos = [[0, 0], [-1, 1], [-1, -1], [1, 1], [1, -1]]
 grid_color = np.array([0.25, 0.25, 0.25])
