9c0fe68d22fb338fd70ff1175dafce1215330c5b
samantha <1342061060@qq.com>
2019-10-31 11:28:27 +0800

    parse_args lists

--- execute_all.py
+++ execute_all.py
@@ -1,2 +1,9 @@
 import os
-os.system("python ./MAControl/Util/TempTest.py")
+os.system("python ./MAControl/Util/TempTest.py --numU=20 "
+          "--p1=0.1 --p1=0.2 --p1=0.7 "
+          "--p2=0.1 --p2=0.2 --p2=0.7 "
+          "--p3=0.1 --p3=0.2 --p3=0.7 "
+          "--q1=0.1 --q1=0.2 --q1=0.7 "
+          "--q2=0.1 --q2=0.2 --q2=0.7 "
+          "--q3=0.1 --q3=0.2 --q3=0.7 "
+          "--typeT=1 --typeT=1 --typeT=1 --typeT=1 --typeT=2 --typeT=3 --typeT=1 --typeT=1 --typeT=1 --typeT=1 ")
