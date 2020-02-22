38341d26229587652fe909c6d56f8e074b33a7a8
samantha <1342061060@qq.com>
2020-02-22 20:26:36 +0800

    TargetProfile

--- MAEnv/scenarios/TargetProfile.py
+++ MAEnv/scenarios/TargetProfile.py
@@ -1,10 +1,11 @@
 import numpy as np
 
+# UAV
+UAV_color = np.array([0.47, 0.79, 0.79])
+
 # target parameters
 num_targets = 0
-target_value = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 target_size = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
-target_defence = [5, 3, 1, 1, 1, 1, 1, 1, 1, 1]
 target_pos = [[1.2,     1],
               [0.5,  -1.5],
               [-1.7,  0.4],
@@ -15,19 +16,11 @@ target_pos = [[1.2,     1],
               [-1.2, -0.8],
               [1.6,  -0.6],
               [0.6,  -1.2]]
-
-# movable target parameters
-m_num_targets = 0
-m_target_value = [7, 5, 2, 3, 5]
-m_target_size = [2, 2, 2, 2, 2]
-m_target_defence = [1, 1, 1, 1, 1]
-m_target_pos = [[-0.8, -0.9], [-0.4, -0.9], [0, -0.9], [0.4, -0.9], [0.8, -0.9]]
-m_target_color = np.array([0, 0.8, 0])
+target_color = np.array([0, 0.8, 0])
 
 # edge of the battle field
 edge = 2
+num_grids = 5
+grid_size = 0.005
 grid_pos = [[0, 0], [edge, edge], [-edge, edge], [-edge, -edge], [edge, -edge]]
 grid_color = np.array([0.25, 0.25, 0.25])
-
-agent_color = np.array([0.47, 0.79, 0.79])
-# agent_color = np.array([0.9, 0.3, 0.5])
