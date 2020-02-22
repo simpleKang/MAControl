9f3a0d6c96403a915184bc72a92e62ba11a649f6
Xiaoxuan233 <Github666>
2020-02-22 16:37:29 +0800

    delete dqn

--- MAControl/Test_Auction/PolicyMaker_SelfOrganization.py
+++ MAControl/Test_Auction/PolicyMaker_SelfOrganization.py
@@ -5,28 +5,18 @@ import math
 import random
 import operator
 
-# [UP or DOWN, RIGHT or LEFT]
-action_dict = {"0": [0., 1.],    # u
-               "1": [1., 1.],    # u r
-               "2": [1., 0.],    #   r
-               "3": [1., -1.],   # d r
-               "4": [0., -1.],   # d
-               "5": [-1., -1.],  # d l
-               "6": [-1., 0.],   #   l
-               "7": [-1., 1.]}   # u l
 
-
-class PolicyMaker_LocalDecision(PolicyMaker):
+class PolicyMaker_SelfOrganization(PolicyMaker):
 
     def __init__(self, name, env, world, agent_index, arglist):
-        super(PolicyMaker_LocalDecision, self).__init__(name, env, world, agent_index, arglist)
+        super(PolicyMaker_SelfOrganization, self).__init__(name, env, world, agent_index, arglist)
         self.opt_index = 0                    # 操作数, 目前有0 / 1 / 10
         self.decision = 0                     # 决策内容
         self.friends_in_sight = list()        # 决策前存储视野中友军
         self.current_friends = list()         # 当前视野中友军
         self.wait_step = -1                   # 决定开始决策前的等待步长
         self.init_step = 300                  # 初始前XX步内不进行任何决策
-        self.after_decision_step = 100         # 决策后XX步内不进行任何决策
+        self.after_decision_step = 100        # 决策后XX步内不进行任何决策
         self.num = arglist.agent_num          # 小瓜子数量
         self.is_decision = False
         self.state = np.zeros(self.num*2)
@@ -63,52 +53,10 @@ class PolicyMaker_LocalDecision(PolicyMaker):
             if point_in_rec(selfview1, selfview2, selfview3, selfview4, friends_pos):
                 self.current_friends.append(i)
 
-    def unitization(self, obs):
-
-        vel = obs[0:2]
-        length = np.sqrt(vel.dot(vel))
-        v1 = vel / length
-
-        return v1
-
     def make_policy(self, WorldTarget, obs_n, step):
 
-        self.find_objects_in_sight(obs_n)
-
-        self.after_decision_step -= 1
-        self.init_step -= 1
-
-        self.state = np.zeros(self.num*2)
-        self.is_decision = False
         self.opt_index = 0
-        self.decision = 0
-
-        if self.current_friends:
-            is_same_friends = operator.eq(self.current_friends, self.friends_in_sight)
-            if not is_same_friends:
-                self.friends_in_sight = self.current_friends.copy()
-                self.wait_step = step + 3
-
-        if step == self.wait_step and self.init_step < 0 and self.after_decision_step < 0:
-
-            if self.friends_in_sight:
-
-                self.state[0:2] = self.unitization(obs_n[self.index])
-
-                for num in range(len(self.friends_in_sight)):
-                    self.state[(num+1)*2:(num+2)*2] = self.unitization(obs_n[self.friends_in_sight[num]])
-
-                #self.action_index = trainer.choose_action(self.state.reshape(1, len(self.state)))
-                self.action_index = random.randint(0, 7)
-
-                self.decision = action_dict[str(int(self.action_index))]
-
-                self.is_decision = True
-                self.opt_index = 1
-                self.after_decision_step = 100
 
         opt = [self.opt_index, self.decision]
-        # 此轮是否进行决策 / 用于决策的状态 / 计算下一状态的辅助信息 / 决策输出的动作
-        c_dis = [self.is_decision, self.state, self.friends_in_sight, self.action_index]
 
-        return opt, c_dis
+        return opt
--- execute.py
+++ execute.py
@@ -9,7 +9,7 @@ import MAControl.Util.OfflineCoverRate as OCR
 import MAControl.Test_Auction.InnerController_PID as IC_P
 import MAControl.Test_Auction.MotionController_L1_TECS as MC_L
 import MAControl.Test_Auction.PathPlanner_EgdeWaypoint as PP_G
-import MAControl.Test_Auction.PolicyMaker_LocalDecision as PM_L
+import MAControl.Test_Auction.PolicyMaker_SelfOrganization as PM_S
 
 
 def parse_args():
@@ -18,36 +18,10 @@ def parse_args():
 
     # Environment
     parser.add_argument("--scenario", type=str, default="scenario5_dqn", help="name of the scenario script")
-    parser.add_argument("--agent-num", type=int, default=20, help="number of agent")
-    parser.add_argument("--train-step-max", type=int, default=10000, help="number of episodes")
-    parser.add_argument("--episode-step-max", type=int, default=4000, help="maximum episode length")
-    parser.add_argument("--display-step-max", type=int, default=4000, help="number of episodes for displaying")
-    parser.add_argument("--learn-num", type=int, default=50, help="number of learning rounds after collecting data")
-    parser.add_argument("--data-collect-num", type=int, default=1, help="number of data collector")
+    parser.add_argument("--agent-num", type=int, default=10, help="number of agent")
+    parser.add_argument("--display-step-max", type=int, default=1000, help="number of episodes for displaying")
+    parser.add_argument("--data-collect-num", type=int, default=3, help="number of data collector")
     parser.add_argument("--cover-edge", type=int, default=200, help="number of cells of one edge")
-
-    # Core training parameters
-    parser.add_argument("--lr", type=float, default=1e-3, help="learning rate for Adam optimizer")
-    parser.add_argument("--gamma", type=float, default=0.9, help="discount factor")
-    parser.add_argument("--e-greedy", type=float, default=0.9, help="epsilon greedy")
-    parser.add_argument("--e-greedy-max", type=float, default=0.99, help="max of epsilon greedy")
-    parser.add_argument("--e-greedy-increment", type=float, default=1e-3, help="increment of epsilon greedy")
-    parser.add_argument("--batch-size", type=int, default=50, help="sample batch memory from all memory for training")
-    parser.add_argument("--memory-size", type=int, default=1000000, help="memory for training")
-    parser.add_argument("--replace-target-iter", type=int, default=300, help="copy eval net params into target net")
-    parser.add_argument("--use-doubleQ", action="store_false", default=True, help="whether to use double Q net")
-
-    # Checkpointing
-    parser.add_argument("--save-dir", type=str, default="./save_model/model",
-                        help="directory in which training state and model should be saved")
-    parser.add_argument("--save-rate", type=int, default=5,
-                        help="save model once every time this many episodes are completed")
-    parser.add_argument("--load-dir", type=str, default="./save_model/",
-                        help="directory in which training state and model are loaded")
-
-    # Evaluation
-    parser.add_argument("--restore", action="store_true", default=False)
-    parser.add_argument("--display", action="store_true", default=True)
 
     return parser.parse_args()
 
@@ -79,7 +53,7 @@ def get_controller(env, world, arglist):
     for i in range(arglist.agent_num):
         control = list()
 
-        control.append(PM_L.PolicyMaker_LocalDecision("agent_%d" % i, env, world, i, arglist))
+        control.append(PM_S.PolicyMaker_SelfOrganization("agent_%d" % i, env, world, i, arglist))
         control.append(PP_G.PathPlanner_EgdeWaypoint("agent_%d" % i, env, world, i, arglist))
         control.append(MC_L.MotionController_L1_TECS("agent_%d" % i, env, world, i, arglist))
         control.append(IC_P.InnerController_PID("agent_%d" % i, env, world, i, arglist))
@@ -99,7 +73,7 @@ def action(arglist, WorldTarget, obs_n, step, NewController):
     # 小瓜子运动
     for i in range(arglist.agent_num):
 
-        list_i, sight_friends = NewController[i][0]. \
+        list_i = NewController[i][0].\
             make_policy(WorldTarget, obs_n, step)
 
         pointAi, pointBi, finishedi, NewController[i][5], WorldTarget = NewController[i][1].\
@@ -133,42 +107,40 @@ if __name__ == '__main__':
     # Create Controller
     NewController = get_controller(env, world, arglist)
 
-    if arglist.display:
+    for num in range(arglist.data_collect_num):
 
-        for num in range(arglist.data_collect_num):
+        with open(os.path.dirname(__file__) + '/track/para.txt', 'w') as f:
+            f.write(str(arglist.cover_edge)+' '+str(arglist.agent_num)+' '+str(arglist.display_step_max))
 
-            with open(os.path.dirname(__file__) + '/track/para.txt', 'w') as f:
-                f.write(str(arglist.cover_edge)+' '+str(arglist.agent_num)+' '+str(arglist.display_step_max))
+        # 为每个小瓜子创建状态文件
+        for k in range(arglist.agent_num):
+            open(os.path.dirname(__file__) + '/track/agent_%d_track.txt' % k, 'w')
 
-            # 为每个小瓜子创建状态文件
+        obs_n = env.reset()
+        start = time.time()
+
+        for step in range(arglist.display_step_max):
+
+            # 选择动作
+            action_n, worldtarget = action(arglist, worldtarget, obs_n, step, NewController)
+
+            new_obs_n, rew_n, done_n, info_n = env.step(action_n)
+
+            obs_n = new_obs_n
+
+            # 保存每个小瓜子每个step的状态信息
             for k in range(arglist.agent_num):
-                open(os.path.dirname(__file__) + '/track/agent_%d_track.txt' % k, 'w')
+                with open(os.path.dirname(__file__) + '/track/agent_%d_track.txt' % k, 'a') as f:
+                    f.write(str(obs_n[k][0])+' '+str(obs_n[k][1])+' '+str(obs_n[k][2])+' '+str(obs_n[k][3])+'\n')
 
-            obs_n = env.reset()
-            start = time.time()
+            # 画图展示
+            augment_view(arglist, world, NewController)
+            env.render()
+            print('>>> Num', num, '>>>> step', step)
+            time.sleep(0.01)
 
-            for step in range(arglist.display_step_max):
-
-                # 选择动作
-                action_n, worldtarget = action(arglist, worldtarget, obs_n, step, NewController)
-
-                new_obs_n, rew_n, done_n, info_n = env.step(action_n)
-
-                obs_n = new_obs_n
-
-                # 保存每个小瓜子每个step的状态信息
-                for k in range(arglist.agent_num):
-                    with open(os.path.dirname(__file__) + '/track/agent_%d_track.txt' % k, 'a') as f:
-                        f.write(str(obs_n[k][0])+' '+str(obs_n[k][1])+' '+str(obs_n[k][2])+' '+str(obs_n[k][3])+'\n')
-
-                # 画图展示
-                augment_view(arglist, world, NewController)
-                env.render()
-                print('>>> Num', num, '>>>> step', step)
-                time.sleep(0.01)
-
-            time.sleep(1)
-            OCR.calculate_coverage(arglist.cover_edge, arglist.agent_num, arglist.display_step_max, num)
-            end = time.time()
-            interval = round((end - start), 2)
-            print('Time Interval ', interval)
+        time.sleep(1)
+        OCR.calculate_coverage(arglist.cover_edge, arglist.agent_num, arglist.display_step_max, num)
+        end = time.time()
+        interval = round((end - start), 2)
+        print('Time Interval ', interval)
