5482c2fc4b318c6fb0578e330e6286ea70770c31
samantha <1342061060@qq.com>
2020-02-23 10:11:13 +0800

    partially execute.py

--- execute.py
+++ execute.py
@@ -1,10 +1,8 @@
 # coding=utf-8
 
 import argparse
-import numpy as np
 import time
-import os
-import MAControl.Util.OfflineCoverRate as OCR
+import MAEnv.scenarios.TargetProfile as T
 
 import MAControl.Default.InnerController_PID as IC_P
 import MAControl.Default.MotionController_L1_TECS as MC_L
@@ -19,9 +17,9 @@ def parse_args():
     # Environment
     parser.add_argument("--scenario", type=str, default="scenario6_AFIT", help="name of the scenario script")
     parser.add_argument("--uav-num", type=int, default=10, help="number of uav")
-    parser.add_argument("--display-step-max", type=int, default=1000, help="number of episodes for displaying")
-    parser.add_argument("--data-collect-num", type=int, default=3, help="number of data collector")
-    parser.add_argument("--cover-edge", type=int, default=200, help="number of cells of one edge")
+    parser.add_argument("--step-max", type=int, default=1000, help="number of maximum steps")
+    parser.add_argument("--repeat-num", type=int, default=3, help="number of repeat runs")
+
 
     return parser.parse_args()
 
@@ -47,53 +45,60 @@ def make_env(arglist):
 
 def get_controller(env, world, arglist):
 
-    ControllerSet = list()
+    uavController = list()
+    targetController = list()
 
     # 初始化小瓜子
     for i in range(arglist.uav_num):
         control = list()
 
-        control.append(PM_S.PolicyMaker_SelfOrganization("agent_%d" % i, env, world, i, arglist))
-        control.append(PP_G.PathPlanner_EgdeWaypoint("agent_%d" % i, env, world, i, arglist))
-        control.append(MC_L.MotionController_L1_TECS("agent_%d" % i, env, world, i, arglist))
-        control.append(IC_P.InnerController_PID("agent_%d" % i, env, world, i, arglist))
+        control.append(PM_S.PolicyMaker_SelfOrganization("uav_%d" % i, env, world, i, arglist))
+        control.append(PP_G.PathPlanner_EgdeWaypoint("uav_%d" % i, env, world, i, arglist))
+        control.append(MC_L.MotionController_L1_TECS("uav_%d" % i, env, world, i, arglist))
+        control.append(IC_P.InnerController_PID("uav_%d" % i, env, world, i, arglist))
         control.append(False)  # Arriveflag
-        control.append(False)  # Isattacking
 
-        ControllerSet.append(control)
+        uavController.append(control)
 
-    return ControllerSet
+    # 初始化小花生
+    for i in range(T.num_targets):
+        control = list()
+
+        control.append(PM_S.PolicyMaker_SelfOrganization("target_%d" % i, env, world, i, arglist))
+        control.append(PP_G.PathPlanner_EgdeWaypoint("target_%d" % i, env, world, i, arglist))
+        control.append(MC_L.MotionController_L1_TECS("target_%d" % i, env, world, i, arglist))
+        control.append(IC_P.InnerController_PID("target_%d" % i, env, world, i, arglist))
+        control.append(False)  # Arriveflag
+
+        targetController.append(control)
+
+    return uavController, targetController
 
 
-def action(arglist, WorldTarget, obs_n, step, NewController):
+def action(WorldTarget, obs_n, step, ControllerSet):
+    # 将WorldTarget作为action的输入参数是为了统一uav/target的控制形式，后者实际拿来不用
 
     # get action
     action_n = list()
 
-    # 小瓜子运动
-    for i in range(arglist.uav_num):
+    # 小瓜子或小花生的运动
+    for i in range(ControllerSet.__len__()):  # 提取ControllerSet的长度
 
-        list_i = NewController[i][0].\
+        list_i = ControllerSet[i][0].\
             make_policy(WorldTarget, obs_n, step)
 
-        pointAi, pointBi, finishedi, NewController[i][5], WorldTarget = NewController[i][1].\
-            planpath(list_i, obs_n[i], NewController[i][4], step, WorldTarget)
+        pointAi, pointBi, finishedi, tempr, WorldTarget = ControllerSet[i][1].\
+            planpath(list_i, obs_n[i], ControllerSet[i][4], step, WorldTarget)
 
-        acctEi, acclEi, NewController[i][4] = NewController[i][2]. \
+        acctEi, acclEi, ControllerSet[i][4] = ControllerSet[i][2]. \
             get_expected_action(obs_n[i], pointAi, pointBi, step, finishedi)
 
-        actioni = NewController[i][3]. \
+        actioni = ControllerSet[i][3]. \
             get_action(obs_n[i], acctEi, acclEi, step, finishedi)
 
         action_n.append(actioni)
 
-    return action_n, WorldTarget
-
-
-def augment_view(arglist, world, NewController):
-    for i in range(arglist.uav_num):
-        if NewController[i][5]:
-            world.agents[i].attacking = True
+    return action_n
 
 
 if __name__ == '__main__':
@@ -102,24 +107,16 @@ if __name__ == '__main__':
 
     # Create environment
     env, world, worldtarget = make_env(arglist)
-    # 手动输入状态维度 n_features
 
     # Create Controller
-    NewController = get_controller(env, world, arglist)
+    Controllers = get_controller(env, world, arglist)
 
-    for num in range(arglist.data_collect_num):
-
-        with open(os.path.dirname(__file__) + '/track/para.txt', 'w') as f:
-            f.write(str(arglist.cover_edge)+' '+str(arglist.agent_num)+' '+str(arglist.display_step_max))
-
-        # 为每个小瓜子创建状态文件
-        for k in range(arglist.agent_num):
-            open(os.path.dirname(__file__) + '/track/agent_%d_track.txt' % k, 'w')
+    for num in range(arglist.repeat_num):
 
         obs_n = env.reset()
         start = time.time()
 
-        for step in range(arglist.display_step_max):
+        for step in range(arglist.step_max):
 
             # 选择动作
             action_n, worldtarget = action(arglist, worldtarget, obs_n, step, NewController)
@@ -128,19 +125,12 @@ if __name__ == '__main__':
 
             obs_n = new_obs_n
 
-            # 保存每个小瓜子每个step的状态信息
-            for k in range(arglist.agent_num):
-                with open(os.path.dirname(__file__) + '/track/agent_%d_track.txt' % k, 'a') as f:
-                    f.write(str(obs_n[k][0])+' '+str(obs_n[k][1])+' '+str(obs_n[k][2])+' '+str(obs_n[k][3])+'\n')
-
             # 画图展示
-            augment_view(arglist, world, NewController)
             env.render()
             print('>>> Num', num, '>>>> step', step)
             time.sleep(0.01)
 
         time.sleep(1)
-        OCR.calculate_coverage(arglist.cover_edge, arglist.agent_num, arglist.display_step_max, num)
         end = time.time()
         interval = round((end - start), 2)
         print('Time Interval ', interval)
