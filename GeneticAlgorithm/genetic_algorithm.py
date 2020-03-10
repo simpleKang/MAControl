c9d2fc05eb5cdfa965a3f1fc113df783c4e213e7
sb-k <602882890@qq.com>
2020-03-10 12:35:33 +0800

    加了交叉与变异

--- GeneticAlgorithm/genetic_algorithm.py
+++ GeneticAlgorithm/genetic_algorithm.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import random
 
 class GA():
     def __init__(self, arglist):
@@ -49,12 +49,53 @@ class GA():
         pass
 
     # 交叉操作,交叉操作完成后，使得新种群数量与原种群保持一致
-    def crossover(self):
-        pass
+    def crossover(self, individual):
+        child = [0] * len(individual)
+        for i in range(0, len(individual)):
+            parent = random.sample(range(0, len(individual)), 2)
+
+            # 行为原型交叉
+            r1 = random.sample(range(0, 6), 2)
+            if r1[0] > r1[1]:
+                a = r1[0]
+                r1[0] = r1[1]
+                r1[1] = a
+            parent1_part1 = individual[parent[0]][0:30]
+            parent2_part1 = individual[parent[1]][0:30]
+            child1_part1 = parent2_part1[0:r1[0] * 5] + parent1_part1[r1[0] * 5:r1[1] * 5] + parent2_part1[r1[1] * 5:]
+            child2_part1 = parent1_part1[0:r1[0] * 5] + parent2_part1[r1[0] * 5:r1[1] * 5] + parent1_part1[r1[1] * 5:]
+
+            # 行为矩阵交叉
+            r2 = random.sample(range(0, 12), 2)
+            if r2[0] > r2[1]:
+                a = r2[0]
+                r2[0] = r2[1]
+                r2[1] = a
+            parent1_part2 = individual[parent[0]][30:210]
+            parent2_part2 = individual[parent[1]][30:210]
+            child1_part2 = parent2_part2[0:r2[0] * 5] + parent1_part2[r2[0] * 5:r2[1] * 5] + parent2_part2[r2[1] * 5:]
+            child2_part2 = parent1_part2[0:r1[0] * 5] + parent2_part2[r2[0] * 5:r2[1] * 5] + parent1_part2[r2[1] * 5:]
+            child1 = child1_part1 + child1_part2
+            child2 = child2_part1 + child2_part2
+            a = random.random()
+            if a < self.crossover_rate:
+                child[i] = child1
+            else:
+                child[i] = child2
+        individual = individual + child
+
+        return individual
 
     # 变异操作,不改变种群数量
-    def mutate(self):
-        pass
+    def mutate(self, individual):
+        # 采用随机位变异
+        for i in range(0, len(individual)):
+            if random.random() < self.mutation_rate:
+                mun_bit = int(210 * self.mutation_neighborhood)
+                each_bit = random.sample(range(0, 210), mun_bit)
+                for j in range(0, len(each_bit)):
+                    individual[i][each_bit[j]] = 1 if individual[i][each_bit[j]] == 0 else 0
+        return individual
 
     # 将新种群编码为二进制形式
     def encode(self):
