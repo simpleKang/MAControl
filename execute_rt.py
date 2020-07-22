748376ba3c28cda1e47fef08268ca7dde2997de7
samantha <1342061060@qq.com>
2020-07-22 15:24:23 +0800

    pretty

---
+++ execute_rt.py
@@ -0,0 +1,13 @@
+import os
+
+os.system("python ./execute.py --numU=5 "
+          "--p1=0.1  --p1=0.45 --p1=0.45 "
+          "--p2=0.45 --p2=0.1  --p2=0.45 "
+          "--p3=0.45 --p3=0.45 --p3=0.1 "
+          "--q1=1.0 --q1=0.0 --q1=0.0 "
+          "--q2=0.0 --q2=1.0 --q2=0.0 "
+          "--q3=0.0 --q3=0.0 --q3=1.0 "
+          "--typeT=1 --typeT=3 --typeT=1 --typeT=1 --typeT=3 --typeT=2 --typeT=1 --typeT=2 --typeT=2 --typeT=1 ")
+
+
+
