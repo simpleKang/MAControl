90a646c03c336c9d284bbf81aceeedd9bfdaa7c6
samantha <1342061060@qq.com>
2020-07-27 11:39:14 +0800

    TempTest

--- MAControl/Util/TempTest.py
+++ MAControl/Util/TempTest.py
@@ -2,7 +2,8 @@ import argparse
 import jsbsim
 
 fdm = jsbsim.FGFDMExec('.', None)
-fdm.load_script('scripts/c1721.xml')
+# fdm.load_script('scripts/c1721.xml')
+fdm.load_script('E:\\S-Projects\\JSBSim\\jsbsim-Release_Candidate_v1.0.0\\scripts\\c1721.xml')
 fdm.run_ic()
 
 while fdm.run():
