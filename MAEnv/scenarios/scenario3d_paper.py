ba50d397c6e55381b1ebe8becd3680bff57e5ae2
samantha <1342061060@qq.com>
2020-08-21 17:37:35 +0800

    v-north v-east

--- MAEnv/scenarios/scenario3d_paper.py
+++ MAEnv/scenarios/scenario3d_paper.py
@@ -119,12 +119,14 @@ class Scenario(BaseScenario, ABC):
         return rew
 
     def observation(self, agent, world):
-        agent.obs = [agent.__getitem__(prp.altitude_sl_ft), agent.__getitem__(prp.pitch_rad),
-                     agent.__getitem__(prp.roll_rad), agent.__getitem__(prp.heading_deg),
+        agent.obs = [agent.__getitem__(prp.altitude_sl_ft),
+                     agent.__getitem__(prp.pitch_rad), agent.__getitem__(prp.roll_rad), agent.__getitem__(prp.heading_deg),
                      agent.__getitem__(prp.u_fps), agent.__getitem__(prp.v_fps), agent.__getitem__(prp.w_fps),
-                     agent.__getitem__(prp.p_radps), agent.__getitem__(prp.q_radps),
-                     agent.__getitem__(prp.r_radps)]
+                     agent.__getitem__(prp.u_aero_fps), agent.__getitem__(prp.v_aero_fps), agent.__getitem__(prp.w_aero_fps),
+                     agent.__getitem__(prp.v_north_fps), agent.__getitem__(prp.v_east_fps),
+                     agent.__getitem__(prp.p_radps), agent.__getitem__(prp.q_radps), agent.__getitem__(prp.r_radps)]
         # [0] altitude [1] pitch [2] roll [3] heading(yaw)
         # [4] u [5] v [6] w
-        # [7] p [8] q [9] r
+        # [7] u-aero [8] v-aero [9] w-aero [10] v-north [11] v-east
+        # [12] p [13] q [14] r
         return agent.obs
