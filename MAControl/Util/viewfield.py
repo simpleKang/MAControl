06a2d6e415aa027d98b70500e0ec42be03230c71
samantha <1342061060@qq.com>
2020-10-04 11:52:42 +0800

    viewfield

---
+++ MAControl/Util/viewfield.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+def viewfield(x, y, h, pitch, roll, heading, wview, hview):
+
+    #                 heading <-----   (x,y)
+    #                                   /|
+    #                                  / | h
+    #               (pitch ± hview)   /  |
+    # ---------------------------------------------------
+    #               (roll ± wview)
+    #
+
+    k = 1
+
+    return None
