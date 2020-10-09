9433d85a11b761d0d547692c94146826898364ad
samantha <1342061060@qq.com>
2020-10-09 22:04:51 +0800

    pid

--- MAControl/PTMA/InnerController_PID.py
+++ MAControl/PTMA/InnerController_PID.py
@@ -8,8 +8,10 @@ class InnerController_PID(InnerController):
         super(InnerController_PID, self).__init__(name, env, world, agent_index, arglist)
 
         # extra params
-        self.ITerm = 0
-        self.last_error = 0
+        self.ITerm_pitch = 0
+        self.ITerm_roll = 0
+        self.Error_pitch = 0
+        self.Error_roll = 0
 
     def get_action(self, obs, pitch_sp, thr_sp, roll_sp, nav_bearing, step, finishedi):
         # print('inner control')
@@ -20,38 +22,42 @@ class InnerController_PID(InnerController):
 
         else:
 
-            Exp_lateral_acc = Eaccl
-            True_lateral_acc = round(obs[5], 5)
-
             delta_time = self.world.dt
             P_value = 0.8
             I_value = 0.08
             D_value = 0.0
-            Iterm_window = 1
+            ITerm_window = 1
 
-            error = Exp_lateral_acc - True_lateral_acc
-            delta_error = error - self.last_error
-            PTerm = error
-            DTerm = delta_error / delta_time
-            self.ITerm += error * delta_time
-            if self.ITerm < -Iterm_window:
-                self.ITerm = -Iterm_window
-            elif self.ITerm > Iterm_window:
-                self.ITerm = Iterm_window
+            error_pitch = pitch_sp - obs[1]
+            delta_error_pitch = error_pitch - self.Error_pitch
+            PTerm_pitch = error_pitch
+            DTerm_pitch = delta_error_pitch / delta_time
+            self.ITerm_pitch += error_pitch * delta_time
+            if self.ITerm_pitch < -ITerm_window:
+                self.ITerm_pitch = -ITerm_window
+            elif self.ITerm_pitch > ITerm_window:
+                self.ITerm_pitch = ITerm_window
             else:
-                self.ITerm = self.ITerm
-            self.last_error = error
+                self.ITerm_pitch = self.ITerm_pitch
+            self.Error_pitch = error_pitch
 
-            acct = Eacct
-            accl = P_value * PTerm + I_value * self.ITerm + D_value * DTerm
-            vel_vector = np.array(obs[0:2])
-            vel_vector[0] = round(vel_vector[0], 4)
-            vel_vector[1] = round(vel_vector[1], 4)
-            speed = np.sqrt(np.square(vel_vector[0]) + np.square(vel_vector[1]))
-            vel_right_unit = np.array([vel_vector[1], -1 * vel_vector[0]]) / speed
-            acc = acct * vel_vector / speed + accl * vel_right_unit
+            error_roll = roll_sp - obs[2]
+            delta_error_roll = error_roll - self.Error_roll
+            PTerm_roll = error_roll
+            DTerm_roll = delta_error_roll / delta_time
+            self.ITerm_roll += error_roll * delta_time
+            if self.ITerm_roll < -ITerm_window:
+                self.ITerm_roll = -ITerm_window
+            elif self.ITerm_roll > ITerm_window:
+                self.ITerm_roll = ITerm_window
+            else:
+                self.ITerm_roll = self.ITerm_roll
+            self.Error_roll = error_roll
 
-            action = [0, acc[0], 0, acc[1], 0]
+            pitch_action = P_value * PTerm_pitch + I_value * self.ITerm_pitch + D_value * DTerm_pitch
+            roll_action = P_value * PTerm_roll + I_value * self.ITerm_roll + D_value * DTerm_roll
+
+            action = [-roll_action, roll_action, pitch_action, 0, 0]
 
         return action
 
