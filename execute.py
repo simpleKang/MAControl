daabdffe1e9e604e0f2289c93bf56420775775de
samantha <1342061060@qq.com>
2020-11-21 10:13:21 +0800

    thr

--- execute.py
+++ execute.py
@@ -135,7 +135,7 @@ if __name__ == '__main__':
         # command #
         ele = [[] for i in range(arglist.numU)]
         ail_r = [[] for i in range(arglist.numU)]
-        rud = [[] for i in range(arglist.numU)]
+        thr = [[] for i in range(arglist.numU)]
         # state #
         pitch = [[] for i in range(arglist.numU)]
         roll = [[] for i in range(arglist.numU)]
@@ -174,9 +174,9 @@ if __name__ == '__main__':
                 alt[i].append(obs_n[i][0])
                 ele[i].append(action_n[i][2])
                 ail_r[i].append(action_n[i][1])
-                rud[i].append(action_n[i][3])
-                pitch[i].append(obs_n[i][1])
-                roll[i].append(obs_n[i][2])
+                thr[i].append(action_n[i][4])
+                pitch[i].append(obs_n[i][1]/math.pi*180)
+                roll[i].append(obs_n[i][2]/math.pi*180)
                 vel[i].append(math.sqrt(obs_n[i][4]**2+obs_n[i][5]**2+obs_n[i][6]**2))
             # Real-time plotting (matplotlib) for visualized DEBUG
             for i in range(arglist.numU):
@@ -186,7 +186,7 @@ if __name__ == '__main__':
                 ax3.plot(sstep, vel[i], color='y')
                 ax4.plot(sstep, ele[i], color='k')
                 ax4.plot(sstep, ail_r[i], color='m')
-                ax4.plot(sstep, rud[i], color='g')
+                ax4.plot(sstep, thr[i], color='g')
             plt.show()
             plt.pause(0.0001)  # 暂停一瞬
 
