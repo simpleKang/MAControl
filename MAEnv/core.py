[fixed] overflow encountered

MAEnv/core.py
@@ -51,7 +51,7 @@ class Entity(object):
         # color
         self.color = None
         # max speed and accel
-        self.max_speed = None
+        self.max_speed = 10000.0
         self.accel = None
         # state
         self.state = EntityState()
@@ -174,11 +174,11 @@ class World(object):
                 if f_a is not None:
                     if p_force[a] is None:
                         p_force[a] = 0.0
-                    p_force[a] = f_a + p_force[a] 
+                    p_force[a] = f_a + p_force[a]
                 if f_b is not None:
                     if p_force[b] is None:
                         p_force[b] = 0.0
-                    p_force[b] = f_b + p_force[b]        
+                    p_force[b] = f_b + p_force[b]
         return p_force
 
     # integrate physical state
@@ -205,7 +205,7 @@ class World(object):
             agent.state.c = np.zeros(self.dim_c)
         else:
             noise = np.random.randn(*agent.action.c.shape) * agent.c_noise if agent.c_noise else 0.0
-            agent.state.c = agent.action.c + noise      
+            agent.state.c = agent.action.c + noise
 
     # get collision forces for any contact between two entities
     def get_collision_force(self, entity_a, entity_b):

MAEnv/scenarios/scenario_entropy.py
@@ -85,13 +85,13 @@ class Scenario(BaseScenario, ABC):
                 # agent.state.p_vel = np.array([0.01, 0.00])  # 10 米/秒
                 agent.state.p_pos = np.random.uniform(-T.edge, T.edge, world.dim_p)
                 agent.state.p_vel = np.random.uniform(-T.edge, T.edge, world.dim_p)
-                agent.state.p_acc = np.array([0, 0])
+                agent.state.p_acc = np.array([0.0, 0.0])
                 agent.color = T.UAV_color
                 uav_count += 1
             else:
                 agent.state.p_pos = np.array(T.target_pos[i-uav_count])
                 agent.state.p_vel = np.array([0.01, 0.00])  # 10 米/秒
-                agent.state.p_acc = np.array([0, 0])
+                agent.state.p_acc = np.array([0.0, 0.0])
                 if agent.movable:
                     agent.color = T.movable_target_color
                 else: