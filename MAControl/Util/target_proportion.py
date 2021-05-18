d5320f7e4273c2f2a49210487112366d3422787d
samantha <1342061060@qq.com>
2021-05-18 19:55:15 +0800

    sceneB

--- MAControl/Util/target_proportion.py
+++ MAControl/Util/target_proportion.py
@@ -9,23 +9,26 @@ def target_distribute(uav_num, step, loop=0):
     curdir = os.path.dirname(__file__)
     pardir = os.path.dirname(os.path.dirname(curdir))
 
-    txt_name = '/perception-%s-%s-%s.txt' % (str(uav_num), str(step), str(loop))
-    open(pardir + '/track' + txt_name, 'w')
+    txt_name = '/target-info-%s-%s-%s.txt' % (str(uav_num), str(step), str(loop))
+    open(pardir + '/scene_Folder' + txt_name, 'w')
 
+    track = list()
     perception = list()
     for i in range(uav_num):
+        track.append(np.loadtxt(pardir + '/track/uav_%d_track.txt' % i))
         perception.append(np.loadtxt(pardir + '/track/uav_%d_perception.txt' % i))
+
+    target = T.target_pos[0:T.num_targets]
+    p2_array = [[] for i in range(uav_num)]
+    p4_array = [[] for i in range(uav_num)]
 
     for lt in range(0, np.size(perception[-1], 0), 5):  # np.size(A,0) 返回该二维矩阵的行数 # range(a,b,c) 类似 matlab [a:c:b)
 
-        p2_array = list()
-        p4_array = list()
-
         for k in range(uav_num):
-            p2_array.append(1-perception[k][lt][1])
-            p4_array.append(perception[k][lt][3])
+            p2_array[k].append(1-perception[k][lt][1])
+            p4_array[k].append(perception[k][lt][3])
             
-        p2_sum = np.sum(p2_array)
-        p4_sum = np.sum(p4_array)
+        p2_sum = sum([sum(item) for item in p2_array]) / len(p2_array)
+        p4_sum = sum([sum(item) for item in p4_array]) / len(p4_array)
         with open(pardir + '/track' + txt_name, 'a') as c:
             c.write(str(lt) + ' ' + str(p2_sum) + ' ' + str(p4_sum) + '\n')
