09d447c97fb07559d903863b608e6c66bbbf7b0a
samantha <1342061060@qq.com>
2021-05-29 10:25:47 +0800

    gamma

--- MAControl/Util/Draw_box_plot.py
+++ MAControl/Util/Draw_box_plot.py
@@ -4,17 +4,23 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def get_box(data_num):
+def get_box(data_num, tc):
 
     r1 = raw_data(data_num, 'Test1-OK-A-Partial')
     r2 = raw_data(data_num, 'Test2-OK-A-Partial')
     r3 = raw_data(data_num, 'Test3-OK-A-Partial')
     r4 = raw_data(data_num, 'Test4-OK-A-Partial')
 
-    rr1 = np.array([[r1[i][j][199] for j in range(32)] for i in range(8)]).T.reshape((4, 64), order='F')
-    rr2 = np.array([[r2[i][j][199] for j in range(32)] for i in range(8)]).T.reshape((4, 64), order='F')
-    rr3 = np.array([[r3[i][j][199] for j in range(32)] for i in range(8)]).T.reshape((4, 64), order='F')
-    rr4 = np.array([[r4[i][j][199] for j in range(32)] for i in range(8)]).T.reshape((4, 64), order='F')
+    g = 5
+
+    rr1 = np.array([[r1[i][j][199] for j in range(32)] for i in range(g)]).T.reshape((4, 8*g), order='F') if tc == 1 \
+        else np.zeros((4, 8*g))
+    rr2 = np.array([[r2[i][j][199] for j in range(32)] for i in range(g)]).T.reshape((4, 8*g), order='F') if tc == 2 \
+        else np.zeros((4, 8*g))
+    rr3 = np.array([[r3[i][j][199] for j in range(32)] for i in range(g)]).T.reshape((4, 8*g), order='F') if tc == 3 \
+        else np.zeros((4, 8*g))
+    rr4 = np.array([[r4[i][j][199] for j in range(32)] for i in range(g)]).T.reshape((4, 8*g), order='F') if tc == 4 \
+        else np.zeros((4, 8*g))
 
     r = np.concatenate((rr3, rr2, rr1, rr4), axis=1)
     box = pd.DataFrame(r)
@@ -46,12 +52,12 @@ def raw_data(data_num, name):
 def set_group_color(f, k):
 
     for whisker in f['whiskers']:
-        whisker.set(color=plt.get_cmap('summer')(k), alpha=0.5, linewidth=0.3)
+        whisker.set(color=plt.get_cmap('Dark2')(k), alpha=0.5, linewidth=0.3)
     for box in f['boxes']:
-        box.set(color=plt.get_cmap('summer')(k), alpha=0.5, linewidth=0.3)
-        box.set(facecolor=plt.get_cmap('summer')(k), alpha=0.5, linewidth=0.3)
+        box.set(color=plt.get_cmap('Dark2')(k), alpha=0.5, linewidth=0.3)
+        box.set(facecolor=plt.get_cmap('Dark2')(k), alpha=0.5, linewidth=0.3)
     for median in f['medians']:
-        median.set(color=plt.get_cmap('summer')(k), alpha=0.9, linewidth=2)
+        median.set(color=plt.get_cmap('Dark2')(k), alpha=0.9, linewidth=2)
 
 
 if __name__ == '__main__':
@@ -61,21 +67,21 @@ if __name__ == '__main__':
 
     co = [[] for k in range(4)]
     for k in range(4):
-        control_box = get_box(data_num)
+        control_box = get_box(data_num, k+1)
         co[k] = control_box.boxplot(showfliers=False, patch_artist=True, showcaps=False, return_type='dict')
         set_group_color(co[k], k)
 
-    k1_list = [i*64 for i in range(5)]   # actual
-    k2_list = ['', '>>> case 1', '>>> case 2', '>>> case 3', '>>> case 4']  # show
+    k1_list = [i*20 for i in range(9)]   # actual
+    k2_list = ['', r'$\gamma = \frac{1}{3}\pi$', '', r'$\gamma = \frac{2}{3}\pi$',
+               '', r'$\gamma = \pi$', '', r'$\gamma = \frac{4}{3}\pi$', '']  # show
     plt.xticks(k1_list, k2_list)
 
     font = {'family': 'Times New Roman', 'weight': 'normal', 'size': 13}
-    plt.xlabel('Step', font)
     plt.ylabel('Cover-Rate', font)
 
-    plt.xlim((0, 256))
+    plt.xlim((0, 160))
     plt.ylim((0.6, 1))
     curdir = os.path.dirname(__file__)
     pardir = os.path.dirname(os.path.dirname(curdir))
-    plt.savefig(pardir+'/track/-plot-/draw-test.png')
+    plt.savefig(pardir+'/track/-plot-/draw-gamma.png')
     plt.show()
