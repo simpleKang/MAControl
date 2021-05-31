c69f9f90770f669d51da9efaf8aa21cb18a852a6
samantha <1342061060@qq.com>
2021-05-31 16:41:49 +0800

    color_str

--- MAControl/Util/Draw_box_plot2.py
+++ MAControl/Util/Draw_box_plot2.py
@@ -32,16 +32,15 @@ def raw_data(data_num, name):
     return coverage_set
 
 
-def set_group_color(f, gen):
-    k = gen * 40 + 20
+def set_group_color(f, color_s):
 
     for whisker in f['whiskers']:
-        whisker.set(color=plt.get_cmap('ocean')(k), alpha=0.5, linewidth=0.5)
+        whisker.set(color=color_s, alpha=0.5, linewidth=0.5)
     for box in f['boxes']:
-        box.set(color=plt.get_cmap('ocean')(k), alpha=0.5, linewidth=0.3)
-        box.set(facecolor=plt.get_cmap('ocean')(k), alpha=0.5, linewidth=0.3)
+        box.set(color=color_s, alpha=0.5, linewidth=0.3)
+        box.set(facecolor=color_s, alpha=0.5, linewidth=0.3)
     for median in f['medians']:
-        median.set(color=plt.get_cmap('ocean')(k), alpha=0.9, linewidth=2)
+        median.set(color=color_s, alpha=0.9, linewidth=2)
 
 
 if __name__ == '__main__':
@@ -58,9 +57,9 @@ if __name__ == '__main__':
         k = draw[kk]
         control_box = get_box(data_num, folder_co, k)
         co[k] = control_box.boxplot(showfliers=False, patch_artist=True, showcaps=False, return_type='dict')
-        set_group_color(co[k], k)
-        plt.text(161, k*0.06 + 0.1, 'generation ' + str(k), fontsize=10, weight='book',
-                 color=plt.get_cmap('ocean')(k*40+20))
+        color_str = plt.get_cmap('ocean')(k * 40 + 20)
+        set_group_color(co[k], color_str)
+        plt.text(161, k*0.06 + 0.1, 'generation ' + str(k), fontsize=10, weight='book', color=color_str)
 
     k1_list = [i*20 for i in range(11)]  # actual
     k2_list = [i*100 for i in range(11)]  # show
@@ -74,5 +73,5 @@ if __name__ == '__main__':
     plt.ylim((0.07, 1))
     curdir = os.path.dirname(__file__)
     pardir = os.path.dirname(os.path.dirname(curdir))
-    plt.savefig(pardir+'/track/-plot-/draw-k3.png')
+    plt.savefig(pardir+'/track/-plot-/draw-test.png')
     plt.show()
