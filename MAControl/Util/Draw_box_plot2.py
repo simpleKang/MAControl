acd42211292591cca337c97d937a0baa33148d4a
samantha <1342061060@qq.com>
2021-05-31 19:48:33 +0800

    A B C

--- MAControl/Util/Draw_box_plot2.py
+++ MAControl/Util/Draw_box_plot2.py
@@ -58,11 +58,12 @@ if __name__ == '__main__':
         color_str = plt.get_cmap('ocean')(k * 40 + 20)
         set_group_color(co[k], color_str)
         plt.text(161, k*0.06 + 0.1, 'generation ' + str(k), fontsize=10, weight='book', color=color_str)
+        # A k*0.06 + 0.1 # B k*0.004 + 0.027 # C k*0.04 + 0.22
 
     control_box = get_box(1, 'A-RPCBF-A', 0)
     co.append(control_box.boxplot(showfliers=False, patch_artist=True, showcaps=False, return_type='dict'))
     set_group_color(co[-1], 'red')
-    plt.text(131, 0.5, 'RoleProjControlBirdFlock', fontsize=10, weight='book', color='red')
+    plt.text(128, 0.5, 'RoleProjControlBirdFlock', fontsize=10, weight='book', color='red')  # A 0.5 # B 0.047 # C 0.92
 
     k1_list = [i*20 for i in range(11)]  # actual
     k2_list = [i*100 for i in range(11)]  # show
@@ -73,7 +74,7 @@ if __name__ == '__main__':
     plt.ylabel('Cover-Rate', font)
 
     plt.xlim((0, 200))
-    plt.ylim((0.07, 1))
+    plt.ylim((0.07, 1))  # A (0.07, 1) # B (-0.002, 0.055) # C (0.2, 1)
     curdir = os.path.dirname(__file__)
     pardir = os.path.dirname(os.path.dirname(curdir))
     plt.savefig(pardir+'/track/-plot-/draw-test.png')
