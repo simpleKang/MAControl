787a59adf9b3ecb90287d09410fdea5190fd1bd4
samantha <1342061060@qq.com>
2021-06-04 17:30:48 +0800

    EDK RDK EVK RVK POK

--- MAControl/Util/Draw_PM_plot.py
+++ MAControl/Util/Draw_PM_plot.py
@@ -10,28 +10,47 @@ plt.rcParams['figure.dpi'] = 200
 curdir = os.path.dirname(__file__)
 pardir = os.path.dirname(os.path.dirname(curdir))
 
-uav_num = 16
+uav_num = 8
 
 track = []
 for k in range(uav_num):
-    PATH = pardir + '/track/test'
+    PATH = pardir + '/track/A8-pi-E-Par/gen=7 ind=0 num=0'
     track.append(np.loadtxt(PATH + '/uav_%d_track.txt' % k))
 
-step_n = len(track[0])
 EDK = list()
+RDK = list()
+EVK = list()
+RVK = list()
+POK = list()
+
 d_ref = 2*1.0/math.sqrt(uav_num)
-RDK = [[] for i in range(step_n)]
-EVK = [[] for i in range(step_n)]
-RVK = [[] for i in range(step_n)]
-POK = [[] for i in range(step_n)]
+v_ref = 0.04
 
 for lt in range(len(track[0])):
+
     edk = list()
+    rdk = list()
+    evk = list()
+    rvk = list()
+    pok = list()
+
     for i in range(uav_num):
+        lenV_i = np.linalg.norm([track[lt][i][0], track[lt][i][1]])
+        evk.append(abs(lenV_i-v_ref))
+        rvk.append(lenV_i)
         for j in range(uav_num):
-            dist2_ij = (track[lt][i][2]-track[lt][j][2])**2 + (track[lt][i][3]-track[lt][j][3])**2
-            edk.append(abs(math.sqrt(dist2_ij)-d_ref))
+            dist_ij = np.linalg.norm([track[lt][i][2]-track[lt][j][2], track[lt][i][3]-track[lt][j][3]])
+            dotV_ij = np.dot([track[lt][i][0], track[lt][i][1]], [track[lt][j][0], track[lt][j][1]])
+            lenV_j = np.linalg.norm([track[lt][j][0], track[lt][j][1]])
+            edk.append(abs(dist_ij-d_ref))
+            rdk.append(dist_ij)
+            pok.append(dotV_ij/lenV_i/lenV_j)
+
     EDK.append(sum(edk)/uav_num/uav_num/d_ref)
+    RDK.append(max(rdk)/d_ref-min(rdk)/d_ref)
+    EVK.append(sum(evk)/uav_num/v_ref)
+    RVK.append(max(rvk)/v_ref-min(rvk)/v_ref)
+    POK.append(sum(pok)/uav_num/uav_num)
 
 plt.figure(facecolor='w')
 line = plt.gca()
