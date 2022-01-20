05c74bd1bcfa53d5b6e1a8ef8a6f5c1d32ed3af0
samantha <1342061060@qq.com>
2022-01-20 09:08:37 +0800

    RALC G-E batch run  |  MORE TARGETS

--- MAEnv/scenarios/TargetProfile.py
+++ MAEnv/scenarios/TargetProfile.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-num_targets = 10
+num_targets = 60
 
-target_size = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]  # 大小相同
-target_pos = [list((np.random.choice(200, 2)-100)/100) for i in range(10)]
+target_size = [2 for ki in range(60)]  # 大小相同
+target_pos = [list((np.random.choice(200, 2)-100)/100) for i in range(60)]
 
 grid_pos = [[0, 0], [-1, 1], [-1, -1], [1, 1], [1, -1]]
 grid_color = np.array([0.25, 0.25, 0.25])
